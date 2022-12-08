c2d324f3e8ebbbba6553be259b91f1b8686d7b0c
Daniel Wright <Daniel.Wright@ingka.ikea.com>
2022-12-08 07:26:25 +0100

    Day 8

---
+++ src/main/kotlin/Day08.kt
@@ -0,0 +1,103 @@
+private fun day08a(input: List<String>): Int {
+
+    val treesVisible = mutableSetOf<Pair<Int, Int>>()
+    val grid = input.map { it.toCharArray().map { char -> char.digitToInt() } }
+
+    val rowCheckRanges = listOf(grid.indices, grid.indices.reversed())
+    val colCheckRanges = listOf(grid[0].indices, grid[0].indices.reversed())
+
+    grid.indices.forEach { row ->
+        colCheckRanges.forEach { columnRange ->
+            var highestTreeSeen = -1
+            columnRange.forEach { column ->
+                if (grid[row][column] > highestTreeSeen) {
+                    treesVisible.add(Pair(column, row))
+                    highestTreeSeen = grid[row][column]
+                }
+            }
+        }
+    }
+
+    grid.first().indices.forEach { column ->
+        rowCheckRanges.forEach { rowRange ->
+            var highestTreeSeen = -1
+            rowRange.forEach { row ->
+                if (grid[row][column] > highestTreeSeen) {
+                    treesVisible.add(Pair(column, row))
+                    highestTreeSeen = grid[row][column]
+                }
+            }
+        }
+    }
+
+    return treesVisible.size
+}
+
+
+private fun day08b(input: List<String>): Int {
+    val grid = input.map { it.toCharArray().map { char -> char.digitToInt() } }
+
+    var maxScore = 0
+    for (gridRow in 1 until grid.size - 1) {
+        for (gridCol in 1 until grid[1].size - 1) {
+
+            var viewingDistanceUp = -1
+            for (row in gridRow - 1 downTo 0) {
+                if (grid[row][gridCol] >= grid[gridRow][gridCol]) {
+                    viewingDistanceUp = gridRow - row
+                    break
+                }
+            }
+            if (viewingDistanceUp < 0) {
+                viewingDistanceUp = gridRow
+            }
+
+            var viewingDistanceRight = -1
+            for (col in gridCol + 1 until grid[1].size) {
+                if (grid[gridRow][col] >= grid[gridRow][gridCol]) {
+                    viewingDistanceRight = col - gridCol
+                    break
+                }
+            }
+            if (viewingDistanceRight < 0) {
+                viewingDistanceRight = grid[gridRow].size - gridCol - 1
+            }
+
+            var viewingDistanceDown = -1
+            for (row in gridRow + 1 until grid.size) {
+                if (grid[row][gridCol] >= grid[gridRow][gridCol]) {
+                    viewingDistanceDown = row - gridRow
+                    break
+                }
+                if (viewingDistanceDown < 0) {
+                    viewingDistanceDown = grid.size - gridRow - 1
+                }
+            }
+
+            var viewingDistanceLeft = -1
+            for (col in gridCol - 1 downTo 0) {
+                if (grid[gridRow][col] >= grid[gridRow][gridCol]) {
+                    viewingDistanceLeft = gridCol - col
+                    break
+                }
+                if (viewingDistanceLeft < 0) {
+                    viewingDistanceLeft = grid[0].size - gridCol - 1
+                }
+            }
+
+            val result = viewingDistanceUp * viewingDistanceDown * viewingDistanceRight * viewingDistanceLeft
+            if (maxScore < result) {
+                maxScore = result
+            }
+        }
+    }
+
+    return maxScore
+
+}
+
+fun main() {
+    val input = loadFile("./src/main/kotlin/Day08.txt")
+    println(day08a(input))
+    println(day08b(input))
+}
---
+++ src/main/kotlin/Day08v2.kt
@@ -0,0 +1,66 @@
+private fun getGrid(input: List<String>) =
+    input.map { it.toCharArray().map { char -> char.digitToInt() } }
+
+private fun day08a(input: List<String>): Int {
+
+    val treesVisible = mutableSetOf<Pair<Int, Int>>()
+    val grid = getGrid(input)
+
+    val rowCheckRanges = listOf(grid.indices, grid.indices.reversed())
+    val colCheckRanges = listOf(grid[0].indices, grid[0].indices.reversed())
+
+    grid.indices.forEach { row ->
+        colCheckRanges.forEach { columnRange ->
+            var highestTreeSeen = -1
+            columnRange.forEach { column ->
+                if (grid[row][column] > highestTreeSeen) {
+                    treesVisible.add(Pair(column, row))
+                    highestTreeSeen = grid[row][column]
+                }
+            }
+        }
+    }
+
+    grid.first().indices.forEach { column ->
+        rowCheckRanges.forEach { rowRange ->
+            var highestTreeSeen = -1
+            rowRange.forEach { row ->
+                if (grid[row][column] > highestTreeSeen) {
+                    treesVisible.add(Pair(column, row))
+                    highestTreeSeen = grid[row][column]
+                }
+            }
+        }
+    }
+
+    return treesVisible.size
+}
+
+
+private fun day08b(input: List<String>): Int {
+    val g = getGrid(input)
+
+    return (1 until g.size - 1).flatMap { r ->
+        (1 until g[1].size - 1).map { c ->
+            listOf(
+                (r - 1 downTo 0).map { g[it][c] },
+                (r + 1 until g.size).map { g[it][c] },
+                (c + 1 until g[1].size).map { g[r][it] },
+                (c - 1 downTo 0).map { g[r][it] }
+            ).map { ts ->
+                val i = ts.indexOfFirst { it >= g[r][c] }
+                if (i < 0) {
+                    ts.size
+                } else {
+                    i + 1
+                }
+            }.fold(1) { x, y -> x * y }
+        }
+    }.max()
+}
+
+fun main() {
+    val input = loadFile("./src/main/kotlin/Day08.txt")
+    println(day08a(input))
+    println(day08b(input))
+}
