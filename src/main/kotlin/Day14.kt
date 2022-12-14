aa39f33e2fc22c903b9a6656a4dac6e37a13a332
Daniel Wright <Daniel.Wright@ingka.ikea.com>
2022-12-14 07:26:33 +0100

    Day 14

---
+++ src/main/kotlin/Day14.kt
@@ -0,0 +1,14 @@
+private fun part1(input: List<String>): Int {
+    return 0
+}
+
+
+private fun part2(input: List<String>): Int {
+    return 0
+}
+
+fun main() {
+    val input = loadFile("./src/main/kotlin/DayXX.txt")
+    println(part1(input))
+    println(part2(input))
+}
